d2f2840ce039251f9407cdf6d61b76ce8ceff14c
olive247 <clive.unger@gmail.com>
2016-07-18 21:04:31 -0500

    Added all files from tutorial

---
+++ client/src/actions/index.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
+
+const ROOT_URL = 'http://localhost:3090';
+
+export function signinUser({ email, password }) {
+  // Submit email/password to server
+  axios.post(`${ROOT_URL}/signin`, { email, password })
+    .then(response => {
+      // If request is good...
+      // - Update state to indicate user is authenticated
+      dispatch({ type: AUTH_USER });
+      // - Save the JWT token
+      localStorage.setItem('token', response.data.token);
+      // - redirect to the route '/feature'
+      browserHistory.push('/feature');
+    })
+    .catch(() => {
+      // If request is bad...
+      // - Show an error to the user
+      dispatch(authError('Bad Login Info'))
+    });
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  }
+}
+
+export function signoutUser() {
+  localStorage.removeItme('token');
+
+  return {type: UNAUTH_USER,}
+
+}
